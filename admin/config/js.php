1bbd12b850c610fe7e10296bd092b02464004db7
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-10 02:44:57 +0700

    Include a sample admin page

---
+++ admin/config/js.php
@@ -0,0 +1,11 @@
+<!-- jQuery -->
+    <script src="../components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
