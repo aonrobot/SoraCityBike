a46687a4f6995a2f8d8df7f55267b23ef0b952bb
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-11 13:42:17 +0700

    Change TestContent Header ICO

--- admin/pages/TestContent.php
+++ admin/pages/TestContent.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header"><i class="fa fa-dashboard fa-1x"></i> Test Content</h1>
+				<h1 class="page-header"><i class="fa fa-terminal fa-1x"></i> Test Content</h1>
 				<div class="panel-heading">
                 	<h3><?php echo $_GET['p']; ?></h3>
                 	<h4><?php echo 'Parameter -> '.$_POST['content']; ?></h4>
