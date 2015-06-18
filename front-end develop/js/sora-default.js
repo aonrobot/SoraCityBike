b89937fb45af04c19f05aef7549ddc08af4b9abd
big14531 <big14531@hotmail.com>
2015-06-18 21:25:59 +0700

    แก้ไขไอคอน social
    ใส่ link mousein mouseout

--- front-end develop/js/sora-default.js
+++ front-end develop/js/sora-default.js
@@ -3,3 +3,12 @@ function toggle_visibility(id) {
     if (e.style.display == 'block' || e.style.display=='') e.style.display = 'none';
     else e.style.display = 'block';
 }
+
+function logo_mousein(id) {
+    var e = document.getElementById(id);
+    e.src = "img/icon/"+e.id+"-type1.png";
+}
+function logo_mouseout(id) {
+    var e = document.getElementById(id);
+    e.src = "img/icon/"+e.id+"-type2.png";
+}
