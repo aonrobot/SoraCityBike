1eba02d3c96ea686087fe5461fc5c0a42766b2a8
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-19 18:14:03 +0700

    Create basic content form

--- admin/pages/content.php
+++ admin/pages/content.php
@@ -18,7 +18,12 @@
                                     	
                                         <div class="form-group">
                                             <label>Tilte</label>
-                                            <input name="title" class="form-control" placeholder="Enter Title Name">
+                                            <input name="title" class="form-control" placeholder="Enter Content Title">
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input name="title" class="form-control" placeholder="Enter Content Name">
                                         </div>
                                         
                                         <div class="form-group">
@@ -30,20 +35,64 @@
                                             <label>Slug</label>
                                             <input name="slug" class="form-control" placeholder="Enter Slug Name">
                                         </div>
-
+                                        
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select class="form-control">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label>Type</label>
+                                            <select class="form-control">
+                                                <option>Normal</option>
+                                                <option>Story</option>
+                                                <option>News</option>
+                                            </select>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label>Category</label><br>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Checkbox 1
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Checkbox 2
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Checkbox 3
+                                                </label>
+                                            </div>
+                                            
+                                        </div>
 
                                         <div class="form-group">
-                                            <label>Content</label>
-                                            
-                                            <div id="mycanvas">
-											    <div class="row">
-											        <div class="col-md-6"><p>Content</p></div>
-											        <div class="col-md-6"><p>Content</p></div>
-											    </div>
-											</div>
+                                            <h2>Content</h2>
+                                            <!-- Pull in Database from language list -->
+                                            <label>English</label>
+	                                            <div id="mycanvas">
+												    <div class="row">
+												        <!-- Pull in Database from english language content -->
+												    </div>
+												</div>
+											<label>Thai</label>
+	                                            <div id="mycanvas2">
+												    <div class="row">
+												        <!-- Pull in Database from thai language content -->
+												    </div>
+												</div>
 											
-                                        </div>
-                                                      
+                                        </div>                          
                    
                                         <button type="submit" class="btn btn-default">Submit Button</button>
                                         <button type="reset" class="btn btn-default">Reset Button</button>
@@ -67,11 +116,16 @@
 </div>
 <!-- /#page-wrapper -->
 
-<script> 
+<script>
+
+	//Loop to create
     $(document).ready(function(){
-        $("#mycanvas").gridmanager();
-        
+        $("#mycanvas").gridmanager();  
     });
+    $(document).ready(function(){
+        $("#mycanvas2").gridmanager();  
+    });
+    //
     
     $( "#save_content" ).click(function() {
 	  alert( "Handler for .click() called." );
