6ef782163a013f3550b9938e822f411628a9c850
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-20 01:34:31 +0700

    Basic Select All content

--- admin/pages/content.php
+++ admin/pages/content.php
@@ -3,9 +3,62 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-12">
-				<!-- Start Content -->				
+				<!-- Start Content -->
 				
 				<h1 class="page-header"><i class="fa fa-book fa-1x"></i> Content Management</h1>
+				
+				<?php if(!strcmp($_GET['a'], 'list')){?>
+				    
+                <div class="panel panel-default">
+                        <div class="panel-heading">
+                            DataTables Advanced Tables
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="dataTable_wrapper">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th>Name</th>
+                                            <th>Author</th>
+                                            <th>Slug</th>
+                                            <th>Status</th>
+                                            <th>Type</th>
+                                            <th>Modified</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                            $datas = $database->select("content", array('cont_title','cont_name','cont_author','cont_slug','cont_status','cont_type','cont_modified'));           
+                                            foreach ($datas as $data) {
+                                    ?>            
+                                        <tr>
+                                            <td><?php echo $data['cont_title'];?></td>
+                                            <td><?php echo $data['cont_name'];?></td>
+                                            <td><?php echo $data['cont_author'];?></td>
+                                            <td><?php echo $data['cont_slug'];?></td>
+                                            <td class="center"><?php echo $data['cont_status'];?></td>
+                                            <td class="center"><?php echo $data['cont_type'];?></td>
+                                            <td class="center"><?php echo $data['cont_modified'];?></td>
+                                        </tr>
+                                        
+                                            <?php }?>
+                                    </tbody>
+                                </table>
+                         </div>
+                          <!-- /.table-responsive -->
+                    </div>
+                      <!-- /.panel-body -->
+                        <?php //echo 'data -> '.print_r($datas); ?>
+                </div>
+                 <!-- /.panel -->
+				    
+				<?php }?> 
+				
+				
+				
+				<?php if(!strcmp($_GET['a'], 'add')){?>				
 				
 				<div class="panel panel-default">
                         <div class="panel-heading">
@@ -106,6 +159,7 @@
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
+                <?php }?>
 				
 			</div>
 			<!-- /.col-lg-12 -->
@@ -130,6 +184,11 @@
     $( "#save_content" ).click(function() {
 	  alert( "Handler for .click() called." );
 	});
+	
+	//DataTable
+	$('#dataTables-example').DataTable({
+                responsive: true
+    });
 	
 </script> 
 
