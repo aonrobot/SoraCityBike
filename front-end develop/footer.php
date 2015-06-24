4459835f014935cc2978829a0b8b248b0149ac60
big14531 <big14531@hotmail.com>
2015-06-24 15:01:18 +0700

    เปลี่ยนหน้า index เป็น php
    include หน้าต่างๆเอา

---
+++ front-end develop/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-right">
+        <small>Copyright &copy; Your Website 2014</small>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ front-end develop/slide.php
@@ -0,0 +1,96 @@
+<div class="swiper-container">
+
+    <!-- Wrapper for slides -->
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+           <div class="slidein" > 
+               <img src="img/slide-1.jpg" alt="" > 
+               <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption</h1>	
+                      <p style="margin-left:10px; color:white;"> Hello my name is pusit nice to meet you. This is first image.
+                      </p>		
+                  </span>
+              </div> 
+
+          </div>
+
+
+
+          <div class="swiper-slide">
+           <div class="slidein" > 
+               <img src="img/slide-2.jpg" alt="" > 
+               <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption1</h1>	
+                      <p style="margin-left:10px; color:white;"> This is second image.
+                      </p>		
+                  </span>
+              </div> 
+
+          </div>
+          <div class="swiper-slide">
+
+           <div class="slidein" > 
+               <img src="img/slide-3.jpg" alt="" > 
+               <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption2</h1>	
+                      <p style="margin-left:10px; color:white;"> This is third image.
+                      </p>		
+                  </span>
+              </div> 
+
+          </div>
+          <div class="swiper-slide">
+              <div class="slidein" > 
+                 <img src="img/slide-1.jpg" alt="" > 
+                 <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption3</h1>	
+                      <p style="margin-left:10px; color:white;"> This is 4th image.
+                      </p>		
+                  </span>
+              </div> 
+
+          </div>
+          <div class="swiper-slide">
+              <div class="slidein" > 
+                 <img src="img/slide-2.jpg" alt="" > 
+                 <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption4</h1>	
+                      <p style="margin-left:10px; color:white;"> This is 5th image.
+                      </p>		
+                  </span>
+              </div> 
+
+          </div>
+
+          <div class="swiper-slide ">
+             <div class="slidein" > 
+                 <img src="img/slide-3.jpg" alt="" > 
+                 <span class="caption simple-caption" >
+                  <h3 style="margin-left:10px;">Simple Caption5</h1>	
+                      <p style="margin-left:10px; color:white;"> This is 6th image.
+                      </p>		
+                  </span>
+              </div> 
+          </div>
+
+
+
+      </div>
+
+      <!--navigation buttons-->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <!-- Add Scrollbar -->
+      <div class="swiper-scrollbar"></div>
+
+
+  </div>
+
+
+  <div class="hidden-slider" align="center">
+
+    <div class="hidden-content" id="hideMe" style="display:none;">I want to hide this by pressing the button above</div>
+
+    <button class="bg-gray hidden-btn pero-font btn btn-default" onclick="toggle_visibility('hideMe')">more slide</button> 
+    
+</div>
