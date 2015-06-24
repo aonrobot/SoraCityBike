8e35a646fff1dd8b306681599d9571430a1d568c
big14531 <big14531@hotmail.com>
2015-06-24 15:23:03 +0700

    แยกเป็นไฟล์ php

--- front-end develop/index.php
+++ front-end develop/index.php
@@ -19,36 +19,29 @@
   <!-- Custom CSS -->
   <link href="css/sora-default.css" rel="stylesheet">
 
-  <!-- Fonts -->
-
 </head>
 
 <body>
-  <?php include('top-menu.php'); ?>
-  <?php include('slide.php'); ?>
-  <hr class="mid-line">
-  <?php include('content.php'); ?>
-  <hr style="max-width: 70%;">
-  <?php include('footer.php'); ?>
 
 
+ <?php include('top-menu.php'); ?>
+ <?php include('slide.php'); ?>
+ <hr class="mid-line">
+ <?php include('content.php'); ?>
+ <hr style="max-width: 70%;">
+ <?php include('footer.php'); ?>
 
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-<script src="js/sora-default.js"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Script to Activate the Carousel -->
-<script>
+ <!-- jQuery -->
+ <script src="js/jquery.js"></script>
+ <script src="js/sora-default.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+  <script src="js/swiper.min.js"></script>
+ 
+ <script>
   $('.carousel').carousel({
         interval: 5000 //changes the speed
       })
 </script>
-<!-- Swiper JS -->
-<script src="js/swiper.min.js"></script>
-
-<!-- Initialize Swiper -->
 <script>
   var swiper = new Swiper('.swiper-container', {
     scrollbar: '.swiper-scrollbar',
