a08dccb06674a0fd691fee1950245febc0f15b34
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-25 16:11:46 +0700

    config

---
+++ config/admin_tiny.php
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="components/tinymce/tinymce.min.js"></script>
+<script>
+tinymce.init({
+    selector: "textarea",
+    theme: "modern",
+    height: 400,
+    plugins: [
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+         "save table contextmenu directionality emoticons template paste textcolor youtube"
+   ],
+   content_css: "components\bootstrap\dist\css\bootstrap.min.css",
+   toolbar: "insertfile undo redo | formatselect | fontselect | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview youtube fullpage | forecolor backcolor", 
+ }); 
+</script>
---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+
+	// Include Medoo
+	require_once '../components/medoo.min.php';
+	
+	// Initialize
+	$database = new medoo(array(
+	    'database_type' => 'mysql',
+	    'database_name' => 'sora_db',
+	    'server' => 'localhost',
+	    'username' => 'root',
+	    'password' => 'root',
+	    'charset' => 'utf8'
+	));
+
+	
+	$site_title = 'SORA City Bike';
+	
+	if (isset($_GET['p'])) {
+	
+		$page = $_GET['p'];
+		// Set $pageid to equal the value given in the URL
+	
+	} else {
+	
+		$page = 'home';
+		// Set $pageid equal to 1 or the Home Page.
+	
+	}
+	
+	# Page Setup:
+	
+	
+	# User Setup:
+
+	
+?>
