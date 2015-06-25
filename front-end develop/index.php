089fc1575ba1f897e4fdb2198e916bbe1791ec6a
big14531 <big14531@hotmail.com>
2015-06-25 16:43:32 +0700

    Revert "ลบไฟล์ html"
    This reverts commit e5796677e7d3e161ea313c986d467ea1201240c0.

--- front-end develop/index.php
+++ front-end develop/index.php
@@ -1,7 +1,7 @@
 
 
 
- <?php include('header.php'); ?>
+ <?php include('top-menu.php'); ?>
  <?php include('slide.php'); ?>
  <hr style="max-width:70%;margin-top:2em;">
  <?php include('content.php'); ?>
@@ -15,7 +15,22 @@
  <script src="js/swiper.min.js"></script>
  
 
+<script>
+ var swiper1 = new Swiper('.swiper1', {
 
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        spaceBetween: 0,
+        grabCursor: false,
+
+        nextButton: '.swiper-button-next1',
+        prevButton: '.swiper-button-prev1'
+
+    });
+	
+	
+	
+</script>
 </body>
 
 </html>
