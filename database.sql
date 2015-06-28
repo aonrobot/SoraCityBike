50bd734dba31de46b1c2071dea16bd6da1101ede
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-06-28 18:06:56 +0700

    Last New Database 28.06.2558 - Add Field slide id in cont and cat - Add Table slide data, menu, object

--- database.sql
+++ database.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `sora_db`.`user` (
 CREATE TABLE IF NOT EXISTS `sora_db`.`content` (
   `id` BIGINT(120) NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT(120) NULL,
+  `slide_id` BIGINT(120) NULL,
   `cont_lang_id` BIGINT(120) NULL,
   `cont_name` VARCHAR(120) NULL,
   `cont_author` VARCHAR(120) NULL,
@@ -41,8 +42,7 @@ CREATE TABLE IF NOT EXISTS `sora_db`.`content` (
   `cont_modified` DATETIME NULL,
   `cont_type` VARCHAR(20) NULL,
   `cont_mine_type` VARCHAR(100) NULL,
-  `cont_thumbnail` VARCHAR(45) NULL,
-  `menu_order` INT(11) NULL,
+  `cont_thumbnail` VARCHAR(255) NULL,
   `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   INDEX `user_id_fk_idx` (`user_id` ASC),
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `sora_db`.`content_translation` (
   `lang_id` BIGINT(120) NULL,
   `cont_title` TEXT NOT NULL,
   `cont_content` LONGTEXT NOT NULL,
-  `cont_description` LONGTEXT NOT NULL,
+  `cont_description` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`trans_id`),
   INDEX `cont_id_idx` (`cont_id` ASC),
   INDEX `lang_code_idx` (`lang_id` ASC),
@@ -103,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `sora_db`.`content_meta` (
   INDEX `cont_id_fk_idx` (`cont_id` ASC),
   CONSTRAINT `cont_id_fk`
     FOREIGN KEY (`cont_id`)
-    REFERENCES `sora_db`.`content` (`user_id`)
+    REFERENCES `sora_db`.`content` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -114,6 +114,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sora_db`.`category` (
   `cat_id` BIGINT(120) NOT NULL AUTO_INCREMENT,
+  `slide_id` BIGINT(120) NULL,
   `cat_name` VARCHAR(255) NOT NULL,
   `cat_slug` VARCHAR(200) NULL,
   `cat_type` VARCHAR(45) NOT NULL,
@@ -149,12 +150,58 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sora_db`.`slide` (
   `slide_id` BIGINT(120) NOT NULL AUTO_INCREMENT,
-  `img_url` VARCHAR(255) NULL,
-  `content` TEXT NULL,
-  `content_url` VARCHAR(255) NULL,
-  `type` VARCHAR(45) NOT NULL,
+  `slide_type` VARCHAR(45) NOT NULL,
   `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`slide_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sora_db`.`object`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sora_db`.`object` (
+  `obj_id` BIGINT(120) NOT NULL AUTO_INCREMENT,
+  `obj_name` VARCHAR(120) NULL,
+  `obj_url` VARCHAR(255) NULL,
+  `obj_type` VARCHAR(45) NULL,
+  PRIMARY KEY (`obj_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sora_db`.`menu`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sora_db`.`menu` (
+  `menu_id` BIGINT(120) NOT NULL AUTO_INCREMENT,
+  `obj_id` BIGINT(120) NULL,
+  `parent_id` BIGINT(120) NULL,
+  `menu_order` VARCHAR(45) NULL,
+  PRIMARY KEY (`menu_id`),
+  INDEX `obj_id_fk_idx` (`obj_id` ASC),
+  CONSTRAINT `obj_id_fk`
+    FOREIGN KEY (`obj_id`)
+    REFERENCES `sora_db`.`object` (`obj_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sora_db`.`slide_data`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sora_db`.`slide_data` (
+  `slide_data_id` BIGINT(120) NOT NULL AUTO_INCREMENT,
+  `slide_id` BIGINT(120) NOT NULL,
+  `slide_data_img_url` VARCHAR(255) NOT NULL,
+  `slide_data_content` VARCHAR(200) NULL,
+  `slide_data_url` VARCHAR(255) NULL,
+  PRIMARY KEY (`slide_data_id`),
+  INDEX `slide_ID_fok_idx` (`slide_id` ASC),
+  CONSTRAINT `slide_ID_fok`
+    FOREIGN KEY (`slide_id`)
+    REFERENCES `sora_db`.`slide` (`slide_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
