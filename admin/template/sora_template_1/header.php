995a95da7a433a25d4955ea31039dec1a9072477
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-07-11 20:12:14 +0700

    Fix Site Title Header

--- admin/template/sora_template_1/header.php
+++ admin/template/sora_template_1/header.php
@@ -3,7 +3,7 @@
 	
 	<head>
 	
-		<title><?php echo $page . ' | ' . $site_title; ?></title>
+		<title><?php echo $page . ' | ' .' Admin '. $site_title; ?></title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
