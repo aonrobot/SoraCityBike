7895aaf164efd4077ace707d63fff3dea3bc6bc3
scummyman <bite_demon@hotmail.com>
2015-07-15 19:26:30 +0700

    add show more button
    show more button without image button

--- index.php
+++ index.php
@@ -309,7 +309,7 @@ $lang=$database->select("language",'*');
 
         <!-- ////////////////////////////////////    THIS  IS CONTENT       ///////////////////////////////////////////////////// -->
         <div class="content row">
-         <div class="container head-content" align="center">
+         <div class="container head-content" id="results" align="center">
           <h4 class="pero-font large-font underline ">stories</h4>
           <p class="pero-font">Kinfolk is a slow lifestyle magazine that explores ways for readers to simplify their lives<br>
            cultivate community and spend more time with their friends and family.</p>
@@ -331,16 +331,18 @@ $lang=$database->select("language",'*');
             );
 
             ?>
-
-
-            <?php foreach ($datas as $data ) {
-
+		
+            <?php 
+			
+			$countsto = 0;
+			foreach ($datas as $data ) {
+			
               if ($data['cat_type']=='story') {
-
+				
                 $a=$database->select("content",'*',["id[=]"=>$data['cont_id']]);
-
+			
                 ?>
-
+				
                 <div class="col-md-4 category-box">
                   <img src=<?php echo '"'.$a[0]['cont_thumbnail'].'"';?> class="index-img"/>
                   <p class="pero-font text-header"><?php echo $data["cont_title"]; ?></p>
@@ -350,12 +352,12 @@ $lang=$database->select("language",'*');
                   </p>
                 </div>
 
-                <?php }} ?>
+                <?php }}  ?>
 
 
               </div> 
-
-
+					<center><a href="#" title="" id="results-show-more">show more</a></center>
+		
             </div>
 
             <!-- ////////////////////////////////////    THIS  IS END OF CONTENT       ///////////////////////////////////////////////////// -->
@@ -374,7 +376,8 @@ $lang=$database->select("language",'*');
             </footer>
 
             <!-- jQuery -->
-
+		
+	
 
 
             <script src="components/js/jquery.js"></script>
@@ -382,6 +385,29 @@ $lang=$database->select("language",'*');
             <script src="components/js/bootstrap.min.js"></script>
             <script src="components/js/bootstrap-hover-dropdown.js"></script>
 
-
+				
+                
+             <script>		
+				$(document).ready(function(){
+					var limit =6;
+					var per_page = 6;
+					
+					jQuery('#results> div.category-box:gt('+(limit-1)+')').hide();
+					
+					if (jQuery('#results > div.category-box').length <= limit) {
+						jQuery('#results-show-more').hide();
+					}
+					
+					jQuery('#results-show-more').bind('click', function(event){
+						event.preventDefault();
+						limit += per_page;
+						jQuery('#results > div.category-box:lt('+(limit)+')').show();
+						if (jQuery('#results > div.category-box').length <= limit) {
+							jQuery(this).hide();
+						}
+					});
+				});
+				 </script>   
+                
           </body>
           </html>
