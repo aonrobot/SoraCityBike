31eba5b8718a249999a1ce85349f4976aeb817d7
big14531 <big14531@hotmail.com>
2015-07-22 15:32:10 +0700

    edit bug

--- components/js/sora-default.js
+++ components/js/sora-default.js
@@ -4,26 +4,15 @@ function toggle_visibility(id) {
 	if (e.style.display == 'block' || e.style.display=='') {
 		e.style.display = 'none';
 		e.style.height = '0px';
-		document.getElementById('video_btn').src = "/SoraCityBike/components/img/down-btn.png";
+		document.getElementById('video_btn').src = "/components/img/down-btn.png";
 	}
 	else {
 	e.style.display = 'block';
 	e.style.height = '403px';
-	document.getElementById('video_btn').src = "/SoraCityBike/components/img/up-btn.png";	
+	document.getElementById('video_btn').src = "/components/img/up-btn.png";	
 	 
-	
-	
-	
 	}
 }
 
 
 
-function logo_mousein(id) {
-	var e = document.getElementById(id);
-	e.src = "/SoraCityBike/components/img/icon/"+e.id+"-type1.png";
-}
-function logo_mouseout(id) {
-	var e = document.getElementById(id);
-	e.src = "/SoraCityBike/components/img/icon/"+e.id+"-type2.png";
-}
