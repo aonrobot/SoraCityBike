3f9f7a5ef90111ceca5de249070c2b44516acc75
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2015-07-27 23:53:03 +0700

    Fix Word Again

--- admin/pages/footer.php
+++ admin/pages/footer.php
@@ -46,10 +46,10 @@
                                                         <div class="col-lg-4 form-group">
                                                             <label>Link Target</label>
                                                             <select name="link_target" class="form-control">
-                                                                <option value="_blank">New window</option>
-                                                                <option value="_self">Same frame as it was clicked</option>
-                                                                <option value="_parent">Parent frameset</option>
-                                                                <option value="_top">Full body of the window</option>
+                                                                <option value="_blank">New window (Blank)</option>
+                                                                <option value="_self">Same frame as it was clicked (Self)</option>
+                                                                <option value="_parent">Parent frameset (Parent)</option>
+                                                                <option value="_top">Full body of the window (Top)</option>
                                                             </select>
                                                         </div>
                 
